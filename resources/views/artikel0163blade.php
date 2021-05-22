9f662ce155c5316b76dc9beb1e041a77fd620106
agungprasetyo163 <agungprasetyo59876@gmail.com>
2021-05-22 14:16:30 +0700

    prak1

---
+++ resources/views/artikel0163blade.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+    <title>agung prasetyo 1461900163!</title>
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="/">Home</a>
+        <a class="nav-link" href="/artikel">artikel</a>
+        <a class="nav-link" href="/contact us">contact us</a>
+        
+      </div>
+    </div>
+  </div>
+  </div>
+</nav>
+<div class="container">
+<div class="row">
+<div class="col-10">
+    <h2 class="mt-2"> PHP NATIF</h2>
+    <p class="left">PHP Native adalah sebuah barisan kode asli yang dibuat sendiri oleh programmer tersebut yang di dalamnya terdapat beberapa barisan kode lainnya seperti HTML, CSS, JavaScript hingga sentuhan bootstrap. Ketika programmer junior menggunakan PHP Native maka manfaatnya jauh lebih efektif sebelum menggunakan framework PHP seperti CodeIgniter ataupun Laravel sebab PHP Native adalah basicnya belajar framework.<p>
+    <h2 class="mt-2"> FRAMEWORK</h2>
+    <p class="left">Dibangun menggunakan barisan kode pemrograman PHP, framework sendiri merupakan kerangka kerja yang lebih terstruktur dan kompleks dalam penggunaan membuat website berskala kecil hingga besar. Kumpulan kode PHP yang sudah diatur menjadi terstruktur maka semua pekerjaan yang dilakukan oleh programmer menjadi cepat dan menghemat waktu.<p>
+    <p class="left">Penggunaan PHP Native tidak cocok untuk membuat website besar seperti pemerintahan, online shop dan lain-lain dikarenakan file-file PHP yang dibuat oleh programmer akan dipisah dan memakan waktu yang sangat lama. PHP Native juga tidak memiliki fitur keamanan layaknya CodeIgniter dan Laravel yang mendeteksi dan mencegah serangan XSS, SQLi ataupun CSRF sebaliknya jika ingin ada fitur tersebut maka programmer harus membuat fitur itu sendiri.<p>
+    <p class="left">PHP Native lebih cocok digunakan untuk pemakaian personal atau bisa di implementasikan untuk membuat website sekolah walau sudah adanya framework, PHP Native masih digunakan hingga saat ini walau tidak begitu banyak apalagi untuk mencari pekerjaan yang rata-rata harus memiliki persyaratan kemampuan di bidang framework..<p>
+    <h2 class="mt-2"> KESIMPULAN</h2>
+    <p class="left">PHP Native memang saat ini tidak begitu diminati untuk kalangan programmer yang sudah lama melintang di dunia pemrograman PHP sebab tidak efisien untuk project berskala besar apalagi memakan waktu cukup lama untuk meracik komponen file-file menjadi rapih dan siap digunakan. Walau demikian jika Anda termasuk programmer yang senang membuat web hanya sekedar iseng atau ada tujuan tertentu bukan untuk project besar sepertinya PHP Native masih diperlukan..<p>
+</div>
+</div>
+</div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
